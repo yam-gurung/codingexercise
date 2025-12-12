a60f7e06b5a7a3ab8e9da590fbc6986a6fe00ed6
yam <yam@192.168.4.36>
2025-12-12 07:11:33 -0500

    FizzBuzz class added

--- src/codingexercise/FizzBuzz.java
+++ src/codingexercise/FizzBuzz.java
@@ -6,10 +6,21 @@ public class FizzBuzz {
 		fizzbuzz();
 	}
 
+//iterates through the range of numbers and prints Fizz for multiple of 3, Buzz for multiple 
+	// of 5 and FizzBuzz for multiple of 3 and 5 and the number itself otherwise
 	private static void fizzbuzz() {
-		
+		int n = 1000;
+		for (int i = 1; i <= 1000; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
 	}
-	
-	
 
 }
